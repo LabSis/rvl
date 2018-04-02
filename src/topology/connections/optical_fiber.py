fb45dc80324684377c01cf4f4c26c322881bebad
Germán Parisi <germannparisi@gmail.com>
2018-04-02 16:18:47 -0300

    Mejoro clase OpticalFiber de acuerdo a la especificacion de Connection

--- src/topology/connections/optical_fiber.py
+++ src/topology/connections/optical_fiber.py
@@ -9,7 +9,8 @@ import lib.canvas as canvas
 class OpticalFiber(con.Connection):
 
     def __init__(self):
-        pass
+        self.devices = [None, None]
+        self.interfaces = [None, None]
 
     def get_tool_name(self):
         return "Fibra óptica"
@@ -22,37 +23,37 @@ class OpticalFiber(con.Connection):
 
     def get_object_canvas(self):
         line_canvas = canvas.LineCanvas()
-        line_canvas.set_x(300)
-        line_canvas.set_y(10)
-        line_canvas.set_width(80)
-        line_canvas.set_height(90)
+        line_canvas.set_x(0)
+        line_canvas.set_y(0)
+        line_canvas.set_width(50)
+        line_canvas.set_height(50)
         return line_canvas
 
     def get_device1(self):
-        return self._device1
+        return self.devices[0]
 
     def get_device2(self):
-        return self._device2
+        return self.devices[1]
 
     def get_interface1(self):
-        return self._interface1
+        return self.interfaces[0]
 
     def get_interface2(self):
-        return self._interface2
+        return self.interfaces[1]
 
     def set_device1(self, device1, interface1):
-        self._device1 = device1
-        self._interface1 = interface1
+        self.devices[0] = device1
+        self.interfaces[0] = interface1
 
     def set_device2(self, device2, interface2):
-        self._device2 = device2
-        self._interface2 = interface2
+        self.devices[1] = device2
+        self.interfaces[1] = interface2
 
     def set_interface1(self, interface1):
-        self._interface1 = interface1
+        self.interfaces[0] = interface1
 
     def set_interface2(self, interface2):
-        self._interface2 = interface2
+        self.interfaces[1] = interface2
 
     def __repr__(self):
         return str(self._device1) + ' (' +  str(self._interface1) + ') <-> ' + str(self._device2) + ' (' +  str(self._interface1) + ')'
