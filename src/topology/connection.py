3bdf38eb011ec47ec64ae9b5ef7ff69c9f7cd37e
Germán Parisi <germannparisi@gmail.com>
2018-04-04 01:18:19 -0300

    Arreglo renderizacion del WireConnection

--- src/topology/connection.py
+++ src/topology/connection.py
@@ -22,12 +22,18 @@ class WireConnectionCanvas(ObjectCanvas):
         super().__init__()
         self.initial_device = None
         self.final_device = None
-        self.old_x = self.get_x()
-        self.old_y = self.get_y()
+        self.old_initial_x = self.x
+        self.old_initial_y = self.y
+        self.old_final_x = self.x
+        self.old_final_y = self.y
 
     def move(self, s, device):
         initial_object_canvas = self.initial_device.get_object_canvas()
         final_object_canvas = self.final_device.get_object_canvas()
+        self.old_initial_x = initial_object_canvas.get_xc()
+        self.old_initial_y = initial_object_canvas.get_yc()
+        self.old_final_x = final_object_canvas.get_xc()
+        self.old_final_y = final_object_canvas.get_yc()
 
         if device.get_id() == self.initial_device.get_id():
             # Se mueve el dispositivo inicial, aplico movimiento al inicial.
@@ -44,12 +50,13 @@ class WireConnectionCanvas(ObjectCanvas):
     def repaint(self):
         initial_object_canvas = self.initial_device.get_object_canvas()
         final_object_canvas = self.final_device.get_object_canvas()
-        min_x = min(self.old_x, initial_object_canvas.get_xc(), final_object_canvas.get_xc())
-        min_y = min(self.old_y, initial_object_canvas.get_yc(), final_object_canvas.get_yc())
-        max_x = max(self.old_x, initial_object_canvas.get_xc(), final_object_canvas.get_xc())
-        max_y = max(self.old_y, initial_object_canvas.get_yc(), final_object_canvas.get_yc())
-        repaint_width = max_x - min_x
-        repaint_height = max_y - min_y
+        min_x = min(self.old_final_x, self.old_initial_x, initial_object_canvas.get_xc(), final_object_canvas.get_xc())
+        min_y = min(self.old_final_y, self.old_initial_y, initial_object_canvas.get_yc(), final_object_canvas.get_yc())
+        max_x = max(self.old_final_x, self.old_initial_x, initial_object_canvas.get_xc(), final_object_canvas.get_xc())
+        max_y = max(self.old_final_y, self.old_initial_y, initial_object_canvas.get_yc(), final_object_canvas.get_yc())
+        repaint_width = abs(max_x - min_x)
+        repaint_height = abs(max_y - min_y)
+        print(repaint_width, repaint_height)
         self.canvas.queue_draw_area(min_x - 3, min_y - 3, repaint_width + 6, repaint_height + 6)
 
     def draw(self, w, cr):
@@ -57,8 +64,6 @@ class WireConnectionCanvas(ObjectCanvas):
         cr.move_to(self.x, self.y)
         cr.line_to(self.width + self.x, self.height + self.y)
         cr.stroke()
-        self.old_x = self.x
-        self.old_y = self.y
 
     def contains(self, x, y):
         return False
